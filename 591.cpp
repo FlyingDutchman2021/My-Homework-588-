07fbaa3e2af71d1d0f648d14065d6c7de0468f4b
FlyingDutchman2021 <flyingdutchman2021@icloud.com>
2021-10-10 17:35:25 +0800

    591

--- 591.cpp
+++ 591.cpp
@@ -3,6 +3,26 @@ using namespace std;
 
 int main(){
 
+    string input;
+    double rate=0,numberOfUsers=1;
+    int counter=0;
+
+    while (1){
+        counter =0;
+        numberOfUsers=1;
+        cin>>input;
+        if (input == "-1"){
+            break;
+        }
+        rate = stoi(input)*0.01;
+
+        while (numberOfUsers < 2.5){
+            numberOfUsers *= (1 + rate);
+            counter += 1;
+        }
+        cout<<counter<<endl;
+
+    }
 
 
 
