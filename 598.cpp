6b0252726aacca11e106b990c0779f2816b369d9
FlyingDutchman2021 <flyingdutchman2021@icloud.com>
2021-10-13 11:07:12 +0800

    598 End Of This Section!

--- 598.cpp
+++ 598.cpp
@@ -3,7 +3,31 @@ using namespace std;
 
 int main(){
 
+    int input1, input2;
+    int result=1;
 
+    while(1){
+
+        input1=0,input2=0,result=1;
+
+
+
+        cin>>input1;
+
+        if (input1 == 0){
+            break;
+        }
+
+        cin>>input2;
+
+        for (int i = 0; i < input2; i++)
+        {
+            result = result * input1;
+        }
+
+        cout<<result<<endl;
+        
+    }
 
 
     return 0;
